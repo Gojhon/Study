874b97cea652c8b295ffc246e074bfe83f090d41
John <kimo87@gmail.com>
2022-03-12 17:32:36 +0900

    변경

--- JavaScript/Inflearn/React/reactexaml1/src/App.js
+++ JavaScript/Inflearn/React/reactexaml1/src/App.js
@@ -1,14 +1,16 @@
-
+import React from 'react';
 import './App.css';
+import MyHeader from "./MyHeader"
 
 function App() {
   let name = "고요한"
+  
   return (
-    <div className="App">
-    <header className='App-header'>
-      <h2>안녕 리엑트 {name}</h2>
-      </header>
-    </div>
+   <div className='App'>
+      <MyHeader />
+        <h2>안녕 리엑트 {name}</h2>
+        <b id="bold_text">React.js</b>
+   </div>
   );
 }
 
