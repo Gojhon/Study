1b71da0fcfeff1f6d37070f55e9214111e119fc9
John <kimo87@gmail.com>
2022-03-12 17:42:07 +0900

    Update App.js

--- JavaScript/Inflearn/React/reactexaml1/src/App.js
+++ JavaScript/Inflearn/React/reactexaml1/src/App.js
@@ -1,15 +1,30 @@
 import React from 'react';
-import './App.css';
+//import './App.css';
 import MyHeader from "./MyHeader"
 
 function App() {
   let name = "고요한"
   
+  const style= {
+    App:{
+      backgroundColor : "black"
+    },
+    h2 : {
+      color : "red"
+    },
+    bold_text : {
+      color:"green",
+    },
+  };
+
+  const number = 5 
   return (
-   <div className='App'>
+   <div style={style.App}>
       <MyHeader />
-        <h2>안녕 리엑트 {name}</h2>
-        <b id="bold_text">React.js</b>
+        <h2 style={style.h2}>안녕 리엑트 {name}</h2>
+       <b style={style.bold_text} id="bold_text">
+         {number}는 : {number %2 ===0 ? "짝수":"홀수"}
+       </b>
    </div>
   );
 }
