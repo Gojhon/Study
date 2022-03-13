ad4d64e2cba9c78bf168044de6fcc7250edf3a63
John <kimo87@gmail.com>
2022-03-13 17:31:34 +0900

    카운터

--- JavaScript/Inflearn/React/reactexaml1/src/App.js
+++ JavaScript/Inflearn/React/reactexaml1/src/App.js
@@ -1,15 +1,27 @@
 import React from 'react';
+import Container from './Container';
 import Counter from './Counter';
 //import './App.css';
 import MyHeader from "./MyHeader"
 
 function App() {
-  const number = 5 
+  const number = 5;
+
+  const counterProps ={
+    a:1,
+    b:2,
+    c:3,
+    d:4,
+    e:5,
+  };
+
   return (
+    <Container>
     <div>
       <MyHeader />
-     <Counter/>
+      <Counter {...counterProps}/>
    </div>
+   </Container>
   );
 }
 
--- JavaScript/Inflearn/React/reactexaml1/src/Counter.js
+++ JavaScript/Inflearn/React/reactexaml1/src/Counter.js
@@ -1,11 +1,30 @@
-const Counter = () =>{
+import React,{useState} from "react";
+import OddEvenResult from "./OddEvenResult";
+const Counter = ({initialValue}) =>{
+    console.log(initialValue);
+    const [count , setCount] = useState(initialValue);
+
+    const onIncrease = ()=>{
+        setCount(count + 1 );
+    };
+
+    const onDecrease = ()=>{
+        setCount(count - 1 );
+    };
+
+   
     return (
         <div>
-            <h2>0</h2>
-            <button>+</button>
-            <button>-</button>
+            <h2>{count}</h2>
+            <button onClick={onIncrease}>+</button>
+            <button onClick={onDecrease}>-</button>
+            <OddEvenResult count={count}/>
         </div>
     );
+};
+
+Counter.defaultProps = {
+    initialValue: 0 
 };
 
 export default Counter;
