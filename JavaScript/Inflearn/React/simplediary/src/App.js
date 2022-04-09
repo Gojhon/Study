0b65d9005b4b27eadc52f40f35eb4c4365d97d66
John <kimo87@gmail.com>
2022-04-09 17:04:01 +0900

    useReducer

--- JavaScript/Inflearn/React/simplediary/src/App.js
+++ JavaScript/Inflearn/React/simplediary/src/App.js
@@ -1,14 +1,42 @@
-import { useCallback, useEffect, useMemo, useRef, useState } from "react";
+import { useCallback, useEffect, useReducer, useRef, useState } from "react";
 import "./App.css";
 import DiaryEditor from "./DiaryEditor";
 import DiaryList from "./DiaryList";
 //import Lifecycle from "./Lifecycle";
 
+const reducer = (state, action)=>{
+  switch(action.type)
+  {
+    case 'INIT':
+      {
+        return action.data
+      }
+    case 'CREATE':
+      {
+        const created_date = new Date().getTime();
+        const newItem = {
+          ...action.data,
+          created_date 
+        };
+        return [newItem,...state]
+      }
+    case 'REMOVE':{
+      return state.filter((it)=>it.id !== action.targetId);
+    }
+    case 'EDIT': {
+      return state.map((it)=>it.id === action.targetId?
+      {...it,content : action.newContent} : it);
+    }
+    default : 
+    return state;
+  }
+}
 
 function App() {
 
-  const [data,setData] = useState([])
+  //const [data,setData] = useState([])
 
+  const [data,dispatch] = useReducer(reducer,[])
   const dataId = useRef(0)
 
   const getData = async() =>{
@@ -34,50 +62,38 @@ function App() {
     getData();
   },[]);
   const onCreate = useCallback((author,content,emotion)=>{
-    const created_date = new Date().getTime();
-
-    const newItem ={
-      author,
-      content,
-      emotion,
-      created_date,
-      id : dataId.current,
-    };
+    dispatch(
+      {type:'CREATE',
+    data:{ author,
+           content,
+           emotion,
+           created_date,
+           id : dataId.current
+          }
+        });
+   
     dataId.current +=1;
-    setData((data) =>[newItem,...data]);
-
+ 
   },[]);
 
   const onRemove = useCallback((targetId)=>{
-    setData(data => data.filter((it)=> it.id !== targetId));
+    dispatch({type:"REMOVE",targetId})
+    // console.log(`${targetId}가 삭제 되었습니다.`)
+    // const newDiaryList = data.filter((it)=> it.id!==targetId);
+    // console.log(newDiaryList);
+    // setData(newDiaryList);
   },[]);
 
-  const onEdit = useCallback((targetId,newContent) =>{
-    setData((data)=>
-      data.map((it)=>it.id === targetId ? {...it,content : newContent} : it)
-    );
+  const onEdit =useCallback((targetId,newContent) =>{
+    // setData(
+    //   data.map((it)=>it.id === targetId ? {...it,content : newContent} : it)
+    // );
+    dispatch({type:"EDIT",targetId,newContent})
   },[]);
-
-  const getDiaryAnalysis= useMemo(() =>
-  {
-    console.log('일기 분석시작')
-    const goodCount = data.filter((it)=>it.emotion >=3).length;
-    const badCount = data.length - goodCount;
-    const goodRatio =(goodCount/data.length)*100;
-    
-    return {goodCount, badCount , goodRatio}
-  },[data.length]);
-
-  const {goodCount,badCount,goodRatio} = getDiaryAnalysis;
-
   return (
     <div className="App">
       <h2>일기장</h2>
       <DiaryEditor onCreate={onCreate} />
-      <div>전체 일기 : {data.length}</div>
-      <div>기분 좋은 일기 개수 : {goodCount}</div>
-      <div>기분 나쁜 일기 개수 : {badCount}</div>
-      <div>기분 좋은 일기 비율 : {goodRatio}</div>
       <DiaryList onEdit = {onEdit} onRemove={onRemove} DiaryList={data} />
     </div>
   );
