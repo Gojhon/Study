d5f6d051a78fac4c6de304751ca52f255e221243
DESKTOP-30RJMTC\kimo8 <kimo87@gmail.com>
2022-04-11 01:27:34 +0900

    스터디

---
+++ CSharp/WPF/ObservableCollection/ObservableCollection/Model/PersonName.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ObservableCollection.Model
+{
+    public class PersonName
+    {
+        private string firstName;
+        private string lastName;
+
+        public PersonName(string first, string last)
+        {
+            this.firstName = first;
+            this.lastName = last;
+        }
+
+        public string FirstName
+        {
+            get { return firstName; }
+            set { firstName = value; }
+        }
+
+        public string LastName
+        {
+            get { return lastName; }
+            set { lastName = value; }
+        }
+    }
+
+
+}
---
+++ CSharp/WPF/ObservableCollection/ObservableCollection/ViewModel/NameList.cs
@@ -0,0 +1,21 @@
+﻿using ObservableCollection.Model;
+using System.Collections.ObjectModel;
+
+namespace ObservableCollection.ViewModel
+{
+    //ObservableCollection을 상속하는 클래스를 만들면
+    //View에서 바인딩이 편리해짐.
+    public class NameList : ObservableCollection<PersonName>
+    {
+        public NameList() : base()
+        {
+            Add(new PersonName("안", "창호"));
+            Add(new PersonName("윤", "봉길"));
+            Add(new PersonName("유", "관순"));
+            Add(new PersonName("다카키", "마사오"));
+            Add(new PersonName("오카모토", "미노루"));
+        }
+    }
+
+
+}
