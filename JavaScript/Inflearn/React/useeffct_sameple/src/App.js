296fadc5e59aac798de43777cc8161e5c9d45f23
DESKTOP-30RJMTC\kimo8 <kimo87@gmail.com>
2022-04-21 08:14:40 +0900

    리액트 발표용 코드 작성

--- JavaScript/Inflearn/React/useeffct_sameple/src/App.js
+++ JavaScript/Inflearn/React/useeffct_sameple/src/App.js
@@ -3,30 +3,15 @@ import "./App.css";
 import { useState, useEffect } from "react";
 
 function App() {
-  const [color, setColor] = useState();
+  const [count,setCount] = useState(0);
 
-  const changeColor = (color) => {
-    setColor(color);
-  };
 
   return (
     <div>
-      <div
-        style={{
-          width: "200px",
-          height: "100px",
-          margin: "12px auto",
-          backgroundColor: color,
-          border: "solid 1px #ccc",
-        }}
-      ></div>
-      <div>
-        <button onClick={() => changeColor("#f00")}>Red</button>
-        <button onClick={() => changeColor("#000")}>Black</button>
-        <button onClick={() => changeColor("#0f0")}>Green</button>
-        <button onClick={() => changeColor("#f0f")}>Pink</button>
-
-      </div>
+      {count}
+      <button onClick={() => setCount(count+1)}>+</button>
+       
+      
     </div>
   );
 }
