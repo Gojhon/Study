b5f8f694c4832bababd7d07e0dea657e4b597527
DESKTOP-30RJMTC\kimo8 <kimo87@gmail.com>
2022-04-22 01:19:13 +0900

    1. 튜플 스터디

---
+++ CSharp/Consol/Tuple_Study1/Tuple_Study1/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Tuple_Study1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Tuple<int, string, string>> Character = new List<Tuple<int, string, string>>();
+
+            Character.Add(new Tuple<int, string, string>(14, "타이", "전사"));
+            Character.Add(new Tuple<int, string, string>(21, "포프", "마법사"));
+            Character.Add(new Tuple<int, string, string>(24, "아방", "기사"));
+            Character.Add(new Tuple<int, string, string>(45, "마암", "전사"));
+            Character.Add(new Tuple<int, string, string>(51, "흉켈", "전사"));
+                                            
+            for (int idx = 0; idx < Character.Count; idx++)
+            {
+                Console.WriteLine("{0} : {1} : {2}", Character[idx].Item1, Character[idx].Item2, Character[idx].Item3);
+            }
+
+        }
+    }
+}
