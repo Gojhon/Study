95c8297e3485ca0225ce1a77392828162ac75fdf
DESKTOP-30RJMTC\kimo8 <kimo87@gmail.com>
2022-05-24 21:53:16 +0900

    로그 커밋

--- JavaScript/Inflearn/React/emotion-diary/src/pages/Edit.js
+++ JavaScript/Inflearn/React/emotion-diary/src/pages/Edit.js
@@ -1,5 +1,15 @@
+import { useSearchParams } from "react-router-dom";
+
 const Edit = () =>
 {
+    const [searchParams,setSearchParams] = useSearchParams()
+    
+    const id = searchParams.get("id")
+    console.log("id : ", id);
+
+    const mode = searchParams.get("mode")
+    console.log("mode : ", id);
+
     return (
         <div>
             <h1>Edit</h1>
