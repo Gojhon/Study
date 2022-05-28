8cd3a50b79519a68e243acb3156783a8cb685527
John <kimo87@gmail.com>
2022-05-28 17:40:45 +0900

    프로젝트 기초공사1

--- JavaScript/Inflearn/React/emotion-diary/src/App.js
+++ JavaScript/Inflearn/React/emotion-diary/src/App.js
@@ -7,12 +7,48 @@ import New from './pages/New'
 import Diary from './pages/Diary'
 
 import RouteTest from './componets/RouteTest';
+import Mybutton from './componets/MyButton';
+import Myheader from './componets/MyHeader';
 
 function App() {
+
+  const env = process.env;
+  env.PUBLIC_URL=env.PUBLIC_URL || "";
   return (
     <BrowserRouter>
     <div className="App">
+      <Myheader headText={"App"} 
+      leftChild={
+      <Mybutton text={"왼쪽 버튼"} onClick={() => alert("왼쪽 클릭")}/>
+      }
+      rightChild={
+        <Mybutton
+        text={"오른쪽 버튼"}
+        onClick={() =>alert("오른쪽 클릭")}/>
+      }
+      
+      />
+      
       <h2>App.js</h2>
+      <img src={process.env.PUBLIC_URL + `/assets/emotion1.png`}/>
+      <img src={process.env.PUBLIC_URL + `/assets/emotion2.png`}/>
+      <img src={process.env.PUBLIC_URL + `/assets/emotion3.png`}/>
+      <img src={process.env.PUBLIC_URL + `/assets/emotion4.png`}/>
+      <img src={process.env.PUBLIC_URL + `/assets/emotion5.png`}/>
+      <Mybutton 
+       text={"버튼"} 
+       onClick={() => alert("버튼 클릭")}
+       type={"positive"}/>
+
+      <Mybutton 
+       text={"버튼"} 
+       onClick={() => alert("버튼 클릭")}
+       type={"negative"}/>
+      <Mybutton 
+       text={"버튼"} 
+       onClick={() => alert("버튼 클릭")}
+       />
+
       <Routes>
         <Route path='/' element={<Home/>}/>
         <Route path='/new' element={<New/>}/>
