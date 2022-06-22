28ff906ccceeb7ef72714a9ece575681b775c2a7
DESKTOP-30RJMTC\kimo8 <kimo87@gmail.com>
2022-06-23 01:29:17 +0900

    변경

--- Spring/firstproject/src/main/java/com/example/firstproject/api/ArticleApiController.java
+++ Spring/firstproject/src/main/java/com/example/firstproject/api/ArticleApiController.java
@@ -5,8 +5,11 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.example.firstproject.dto.ArticleForm;
 import com.example.firstproject.entity.Article;
 import com.example.firstproject.repository.ArticleRepository;
 
@@ -26,5 +29,12 @@ public class ArticleApiController {
 	public Article index(@PathVariable Long id){
 		return articleRepository.findById(id).orElse(null);
 		
+	}
+	
+	@PostMapping("/api/articles")
+	public Article create(@RequestBody ArticleForm dto)
+	{
+		Article article = dto.toEntity();
+		return articleRepository.save(article);
 	}
 }
