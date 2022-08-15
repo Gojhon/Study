3b4da72287cd11a79910b481f7a73e7622253689
John <kimo87@gmail.com>
2022-08-16 00:53:03 +0900

    spring boot

--- Spring/firstproject/src/main/java/com/example/firstproject/api/CommentApiController.java
+++ Spring/firstproject/src/main/java/com/example/firstproject/api/CommentApiController.java
@@ -1,5 +1,25 @@
 package com.example.firstproject.api;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
+
+import com.example.firstproject.dto.CommentDto;
+import com.example.firstproject.service.CommentService;
+
 public class CommentApiController {
 
+	@Autowired
+	private CommentService commentService;
+	
+	
+	public ResponseEntity<List<CommentDto>>comments(@PathVariable Long articleId)
+	{
+		List<CommentDto> dtos = commentService.comments(articleId);
+				
+		return ResponseEntity.status(HttpStatus.OK).body(dtos);
+	}
 }
--- Spring/firstproject/src/main/java/com/example/firstproject/service/CommentService.java
+++ Spring/firstproject/src/main/java/com/example/firstproject/service/CommentService.java
@@ -1,5 +1,37 @@
 package com.example.firstproject.service;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+
+import com.example.firstproject.dto.CommentDto;
+import com.example.firstproject.entity.Comment;
+import com.example.firstproject.repository.ArticleRepository;
+import com.example.firstproject.repository.CommentRepository;
+
 public class CommentService {
 
+	@Autowired
+	private CommentRepository commentRepository;
+	
+
+	@Autowired
+	private ArticleRepository articleRepository;
+
+
+	public List<CommentDto> comments(Long articleId) {
+
+		List<Comment> comments = commentRepository.findByArticleId(articleId);
+		
+		List<CommentDto> dtos = new ArrayList<CommentDto>();
+		
+		for(int i=0; i< comments.size(); i++)
+		{
+			Comment c = comments.get(i);
+			CommentDto dto = CommentDto.createCommentDto(c);
+			dtos.add(dto);
+		}
+		return dtos;
+	}
 }
