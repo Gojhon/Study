27507954a7fe9b1eaf80c106f96ef54231943fac
John <kimo87@gmail.com>
2022-09-29 02:19:49 +0900

    스터디

---
+++ Python/study1.py
@@ -0,0 +1,11 @@
+a={'일본','중국','한국'}
+print(type(a))
+a.add('중국')
+print(a)
+a.add('북한')
+print(a)
+a.remove('일본')
+print(a)
+a.update({'홍콩','한국','베트남'})
+
+print(a)
---
+++ Python/study2.py
@@ -0,0 +1,8 @@
+class arr:
+   a=["Seoul","Kyeonggi","Inchon","Daejoen","Daegu","Busan"]
+
+str =' '
+
+for i in arr.a:
+  str = str + i[0]
+print(str)
