90d12f51df25090f5ea88cbf63c7d8bd59b2a265
John <kimo87@gmail.com>
2023-01-07 02:15:45 +0900

    1. API 추가

---
+++ Spring/myhome/src/main/java/com/solidit/myhome/controller/BoardApiController.java
@@ -0,0 +1,77 @@
+package com.solidit.myhome.controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.util.StringUtils;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.solidit.myhome.model.Board;
+import com.solidit.myhome.repository.BoardRepository;
+
+@RestController
+@RequestMapping("/api")
+class BoardApiController {
+
+	
+  @Autowired
+  private  BoardRepository repository;
+
+
+
+
+  // Aggregate root
+  // tag::get-aggregate-root[]
+  @GetMapping("/boards")
+  List<Board> all(@RequestParam(required = false)String title) {
+	  if(StringUtils.isEmpty(title))
+	  {
+		  return repository.findAll();			 
+	  }else
+	  {
+		  return repository.findByTitle(title);
+     }
+  }
+  // end::get-aggregate-root[]
+
+  @PostMapping("/boards")
+  Board newBoard(@RequestBody Board newBoard) {
+    return repository.save(newBoard);
+  }
+
+  // Single item
+  
+  @GetMapping("/boards/{id}")
+  Board one(@PathVariable Long id) {
+    
+    return repository.findById(id).orElse(null);
+  }
+
+  @PutMapping("/boards/{id}")
+  Board replaceBoard(@RequestBody Board newBoard, @PathVariable Long id) {
+    
+    return repository.findById(id)
+      .map(Board -> {
+        Board.setTitle(newBoard.getTitle());
+        Board.setContent(newBoard.getContent());
+        return repository.save(Board);
+      })
+      .orElseGet(() -> {
+        newBoard.setId(id);
+        return repository.save(newBoard);
+      });
+  }
+
+  @DeleteMapping("/boards/{id}")
+  void deleteBoard(@PathVariable Long id) {
+    repository.deleteById(id);
+  }
+}
--- Spring/myhome/src/main/java/com/solidit/myhome/repository/BoardRepository.java
+++ Spring/myhome/src/main/java/com/solidit/myhome/repository/BoardRepository.java
@@ -1,8 +1,12 @@
 package com.solidit.myhome.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import com.solidit.myhome.model.Board;
 
 public interface BoardRepository extends JpaRepository<Board,Long> {
+
+	List<Board> findByTitle(String title);
 
 }
