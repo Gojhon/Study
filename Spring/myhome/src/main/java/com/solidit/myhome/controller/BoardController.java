4488fe3b04549afcc89c328e82130b6fcbca996c
John <kimo87@gmail.com>
2023-01-08 22:41:13 +0900

    study

--- Spring/myhome/src/main/java/com/solidit/myhome/controller/BoardController.java
+++ Spring/myhome/src/main/java/com/solidit/myhome/controller/BoardController.java
@@ -2,6 +2,10 @@ package com.solidit.myhome.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
@@ -26,10 +30,16 @@ public class BoardController {
 	
 	@Autowired
 	private BoardValidator boardValidator;
+	
 	@GetMapping("/list")
-    public String list(Model model) {
-		List<Board> boards = boardRepository.findAll();
+    public String list(Model model,@PageableDefault(size=2)Pageable pageable) {
+		Page<Board> boards = boardRepository.findAll(pageable);
+		//boards.getTotalElements();
+		int startPage = Math.max(1, boards.getPageable().getPageNumber() - 4);
+	    int endPage = Math.min(boards.getTotalPages(), boards.getPageable().getPageNumber() + 4);
 		model.addAttribute("boards",boards);
+		model.addAttribute("startPage", startPage);
+        model.addAttribute("endPage", endPage);
         return "board/list";
     }
 	
