72829a67033d65b0641fcc128e493d0191cdbe0a
John <kimo87@gmail.com>
2023-01-26 01:37:42 +0900

    1. DB 연결

--- Python/myapi/database.py
+++ Python/myapi/database.py
@@ -4,9 +4,11 @@ from sqlalchemy.orm import sessionmaker
 
 SQLALCHEMY_DATABASE_URL =  "mysql+pymysql://root:python3@14.7.11.165:1173/kimoapi"
 #SQLALCHEMY_DATABASE_URL = "sqlite:///./myapi.db"
-engine = create_engine(
-    SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False}
-)
+# engine = create_engine(
+#     SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False}
+# )
+
+engine = create_engine(SQLALCHEMY_DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
 Base = declarative_base()
--- Spring/myhome/src/main/java/com/solidit/myhome/config/WebSecurityConfig.java
+++ Spring/myhome/src/main/java/com/solidit/myhome/config/WebSecurityConfig.java
@@ -1,18 +1,20 @@
 package com.solidit.myhome.config;
 
 
-import javax.sql.DataSource;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.provisioning.InMemoryUserDetailsManager;
-import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+import javax.sql.DataSource;
+
 
 @Configuration
 @EnableWebSecurity
@@ -22,20 +24,38 @@ public class WebSecurityConfig {
 	private DataSource dataSource;
 	
 	@Bean
-	public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+	protected void configure(HttpSecurity http) throws Exception {
 		http
 			.authorizeHttpRequests((requests) -> requests
-				.requestMatchers("/", "/home").permitAll()
+				.requestMatchers("/").permitAll()
 				.anyRequest().authenticated())
 			.formLogin((form) -> form
 				.loginPage("/login")
 				.permitAll())
 			.logout((logout) -> logout.permitAll());
 
-		return http.build();
 	}
 
+	@Autowired
+	public void configureGlobal(AuthenticationManagerBuilder auth) 
+	  throws Exception {
+	    auth.jdbcAuthentication()
+	      .dataSource(dataSource)
+	      .passwordEncoder(passwordEncoder())
+	      .usersByUsernameQuery("select username, email,password,enabled "
+	        + "from user "
+	        + "where username = ?")
+	      .authoritiesByUsernameQuery("select username, name "
+	        + "from user_role ur inner join user u on ur.user_id = u.id "
+	        + "inner join role r on ur.role_id = r.id "
+	        + "where email = ?");
+	}
+	
+	@Bean
+	public PasswordEncoder passwordEncoder() {
+	    return new BCryptPasswordEncoder();
 
+	}
 }
 
 
