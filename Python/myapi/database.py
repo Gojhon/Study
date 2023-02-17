c2e725ab127850611e6cef8e709d943bbc99e571
John <kimo87@gmail.com>
2023-02-18 08:22:53 +0900

    파이썬 스터디

--- Python/myapi/database.py
+++ Python/myapi/database.py
@@ -1,6 +1,7 @@
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
+import contextlib
 
 SQLALCHEMY_DATABASE_URL =  "mysql+pymysql://root:python3@14.7.11.165:1173/kimoapi"
 #SQLALCHEMY_DATABASE_URL = "sqlite:///./myapi.db"
@@ -12,3 +13,11 @@ engine = create_engine(SQLALCHEMY_DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
 Base = declarative_base()
+
+@contextlib.contextmanager
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
