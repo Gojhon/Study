752c1d60c8d3fc3d24f18f7191f174bd76d21f26
John <kimo87@gmail.com>
2023-02-18 20:19:02 +0900

    WPF

---
+++ WPF/1Day/WpfApp1/FormsMain/KimoViewModel.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FormsMain
+{
+    public class KimoViewModel
+    {
+
+        public string Name { get; set; }
+        public bool IsTest { get; set; }
+
+        public KimoViewModel()
+        {
+            Name = "kimo";
+            IsTest = true;
+        }
+
+    }
+}
