4ac7c8ab669d380d18f3c4904ea39b21134b2a8c
John <kimo87@gmail.com>
2023-03-05 13:58:11 +0900

    WPF 스터디

---
+++ WPF/1Day/WpfApp3/WpfApp3/App.cs
@@ -0,0 +1,20 @@
+﻿using FormMain;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace WpfApp3
+{
+    internal class App : Application
+    {
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            base.OnStartup(e);
+            Window1 win = new();
+            win.ShowDialog();
+        }
+    }
+}
